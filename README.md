542416f87b991f30fab02200ac09b8c12270a7d2
Alberto Leal <mailforalberto@gmail.com>
2017-10-22 05:09:39 -0400

    exports script

--- README.md
+++ README.md
@@ -3,7 +3,15 @@ react-resume
 
 > Generate resumes using [React](https://github.com/facebook/react) and [puppeteer](https://github.com/GoogleChrome/puppeteer).
 
+Inspired by: https://github.com/salomonelli/best-resume-ever
 
+Usage
+=====
+
+1. Install deps: `yarn install`
+2. Start server: `yarn start`
+3. Edit `src/`.
+4. Export PDF: `yarn pdf`
 
 License
 =======
---
+++ scripts/export.js
@@ -0,0 +1,85 @@
+// node imports
+
+const http = require("http");
+const path = require("path");
+
+// 3rd-party imports
+
+const mkdirp = require("mkdirp");
+const puppeteer = require("puppeteer");
+
+// export code
+
+const SERVER = "http://localhost:3000/";
+const OUT_DIR = path.join(__dirname, "../out/");
+const PDF_FILENAME = "resume.pdf";
+
+const fetchResponse = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            const request = http.request(SERVER, response =>
+                resolve(response.statusCode)
+            );
+            request.on("error", err => reject(err));
+            request.end();
+        } catch (err) {
+            reject(err);
+        }
+    });
+};
+
+const reachableServer = () => {
+    console.log("Connected to server ...");
+
+    return fetchResponse().then(statusCode => {
+        if (statusCode === 200) {
+            // 200 OK
+            return true;
+        }
+
+        throw Error(
+            `Unable to connect to server. Received status code: ${statusCode}`
+        );
+    });
+};
+
+const makeOutDir = () => {
+    return new Promise((resolve, reject) => {
+        mkdirp(OUT_DIR, err => {
+            if (!err) {
+                resolve();
+                return;
+            }
+
+            reject(err);
+        });
+    });
+};
+
+const convert = async () => {
+    await reachableServer();
+
+    console.log("Exporting ...");
+
+    const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
+
+    const page = await browser.newPage();
+
+    await page.goto(SERVER, { waitUntil: "networkidle" });
+
+    await makeOutDir();
+
+    await page.pdf({
+        path: path.join(OUT_DIR, PDF_FILENAME),
+        format: "A4"
+    });
+
+    await browser.close();
+
+    console.log("Finished exports.");
+};
+
+convert().catch(reason => {
+    console.error(`${reason}`);
+    process.exit(1);
+});
