344317435b42d0648a5275ff6b032a9311420a10
Alberto Leal <mailforalberto@gmail.com>
2017-10-27 00:15:47 -0400

    whitespace

--- src/components/daterange.js
+++ src/components/daterange.js
@@ -17,10 +17,15 @@ const DateRange = ({ start, end = NOT_SET }) => {
     invariant(start, "expected start");
 
     if (end === NOT_SET) {
-        return <Container>{`${start} \u2014 Present`}</Container>;
+        return (
+            <Container
+            >{`${start}\u00A0\u00A0\u2014\u00A0\u00A0Present`}</Container>
+        );
     }
 
-    return <Container>{`${start} \u2014 ${end}`}</Container>;
+    return (
+        <Container>{`${start}\u00A0\u00A0\u2014\u00A0\u00A0${end}`}</Container>
+    );
 };
 
 export default DateRange;
